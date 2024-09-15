45754439d4cc10fd633a36f4c24e4afdddbf4a94
Matthew-19 <113844755+Matthew-19@users.noreply.github.com>
2024-09-15 14:45:17 +0300

    connected to local database

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
 app.use(express.json());
 
 const coursesRouter = require("./src/courses/courses.services");
 app.use("/api/courses", coursesRouter);
+
+const db_uri = "mongodb://127.0.0.1:27017/courses";
+mongoose
+  .connect(db_uri)
+  .then(() => console.log("connected to database"))
+  .catch((err) => console.log("database connection err: ", err));
 
 app.listen(5000, () => {
   console.log("listening on port 5000");
--- src/courses/courses.controller.js
+++ src/courses/courses.controller.js
@@ -1,72 +1,80 @@
 let { courses } = require("../courses/courses.data");
+const Course = require("../../models/courses.model");
 
-const getAllCourses = (req, res) => {
-  res.status(200).json(courses);
-};
+const queryProjection = { __v: false };
 
-const getCourse = (req, res) => {
-  const id = +req.params.id;
-  const course = courses.find((course) => course.id === id);
-
-  if (course) {
-    res.status(200).json(course);
-  } else {
-    res.status(404).json({ msg: "course not found" });
+const getAllCourses = async (req, res) => {
+  try {
+    const courses = await Course.find({}, queryProjection);
+    res.status(200).json({ status: "success", data: courses });
+  } catch (err) {
+    res.json({ status: "fail", data: err });
   }
 };
 
-const addCourse = (req, res) => {
-  const name = req.body.name.length
-    ? req.body.name
-    : `course ${courses.length + 1}`;
-  const price = +req.body.price;
-  const id = courses.length + 1;
-  const newCourse = {
-    id: id,
-    name: name,
-    price: price,
-  };
-
-  if (isNaN(price))
-    return res.status(400).json({ error: "price must be a number" });
-
-  courses.push(newCourse);
-  res.status(201).json(newCourse);
+const getCourse = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const course = await Course.findById(id, queryProjection);
+    res.status(200).json({ state: "success", data: course });
+  } catch (err) {
+    res.status(404).json({ state: "fail", data: err });
+  }
 };
 
-const deleteCourse = (req, res) => {
-  const id = +req.params.id;
-  if (isNaN(id)) return res.status(400).json({ error: "id must be a number" });
-  courses = courses.filter((course) => course.id !== id);
-  res.status(200).json(courses);
+const addCourse = async (req, res) => {
+  try {
+    const courses = await Course.find({}, queryProjection);
+    const name = req.body.name.length
+      ? req.body.name
+      : `course ${courses.length + 1}`;
+    const price = req.body.price;
+    const newCourse = new Course({ name: name, price: price });
+    await newCourse.save();
+
+    res.status(201).json({ status: "success", data: newCourse });
+  } catch (err) {
+    res.status(400).json({ status: "fail", data: err });
+  }
 };
 
-const updateCourse = (req, res) => {
-  const id = +req.params.id;
-  let name = req.body.name ? req.body.name : undefined;
-  let price = req.body.price ? +req.body.price : undefined;
+const deleteCourse = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const course = await Course.findById(id);
+    if (course) {
+      await Course.deleteOne({ _id: id });
+      res.status(200).json({
+        status: "success",
+        data: { msg: "course deleted successfully" },
+      });
+    } else {
+      res
+        .status(400)
+        .json({ status: "fail", data: { msg: "course does not exist!" } });
+    }
+  } catch (err) {
+    res.status(400).json({ status: "fail", data: err });
+  }
+};
 
-  let updatedCourse = courses.find((course) => course.id === id);
-  if (!updatedCourse)
-    return res.status(400).json({ error: "course not found" });
-
-  if (name)
-    if (name.length > 2) updatedCourse = { ...updatedCourse, name: name };
-    else
-      return res.status(400).json({ error: "name must be more than 2 chars" });
-
-  if (price)
-    if (isNaN(price))
-      return res.status(400).json({ error: "price must be a number" });
-    else updatedCourse = { ...updatedCourse, price: price };
-
-  if (name === undefined && price === undefined)
-    res.status(400).json({ error: "Need either name or price" });
-  else {
-    courses = courses.map((course) =>
-      course.id === id ? updatedCourse : course
+const updateCourse = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const oldCourse = await Course.findById(id);
+    const updatedCourse = await Course.updateOne(
+      { _id: id },
+      {
+        $set: {
+          name: oldCourse.name,
+          price: oldCourse.price,
+          ...req.body,
+        },
+      }
     );
-    res.status(200).json(updatedCourse);
+    res.status(200).json({ status: "success", data: updatedCourse });
+  } catch (err) {
+    res.status(400).json({ status: "fail", data: err });
   }
 };
 
